7609618b47cb6e887f8ea24ee922770874f2c457
niyaschelari123 <niyaschelari11@gmail.com>
2026-09-26 20:18:57 +0530

    changes

--- src/game/socket.ts
+++ src/game/socket.ts
@@ -4,10 +4,10 @@ import { io, type Socket } from 'socket.io-client'
 // const SOCKET_URL = 'https://ludo-w7dl.onrender.com'
 
 // in niyas.rio123 render deployment https://ludokingfinal.niyas-c.workers.dev/
-// const SOCKET_URL = 'https://ludo-1-7e6c.onrender.com' 
+const SOCKET_URL = 'https://ludo-1-7e6c.onrender.com' 
 
 // in niyaschelari123 render deployment https://ludokingclone.niyas-c.workers.dev/
-const SOCKET_URL = 'https://ludo-23xe.onrender.com'
+// const SOCKET_URL = 'https://ludo-23xe.onrender.com'
 
 // const SOCKET_URL = 'http://localhost:3001'
 
